d0d181b883698bc5cd922bd1dfac39ea811c9637
devxml <devverma2031@gmail.com>
2026-08-29 13:24:37 +0530

    Added Solution - LeetCode to GitHub

---
+++ binary_tree/Kth Smallest Element in a BST/code.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = -1;
+
+    void inorder(TreeNode* node, int& k) {
+        if (!node || k == 0) return;
+
+        inorder(node->left, k);
+
+        k--;
+        if (k == 0) {
+            ans = node->val;
+            return;
+        }
+
+        inorder(node->right, k);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root, k);
+        return ans;
+    }
+};
